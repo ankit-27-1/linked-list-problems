3d610f5ce88aecc44742de6fc8178588abc070a5
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-04 23:38:27 +0530

    Create itrSearch.java
    Searching a node in linked list using iterative search.

---
+++ itrSearch.java
@@ -0,0 +1,12 @@
+static int itrSearch(int key){
+        Node temp=head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data==key){
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }
+        return -1;
+    }
