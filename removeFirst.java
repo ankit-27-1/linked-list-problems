c331487285557924d6d9accd367b2915b1974fd9
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-04 23:40:31 +0530

    Create removeFirst.java
    Removing the first node from linked list.

---
+++ removeFirst.java
@@ -0,0 +1,16 @@
+public int removeFirst(){
+        if(size==0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
