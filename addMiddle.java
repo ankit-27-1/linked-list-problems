4f45a53988707bc29152e62f81b348edc4bb09e4
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-04 23:41:13 +0530

    Create addMiddle.java
    Adding a node in the middle of linked list.

---
+++ addMiddle.java
@@ -0,0 +1,12 @@
+public void addMiddle(int idx,int data){
+        Node newNode=new Node(data);
+        size++;
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
