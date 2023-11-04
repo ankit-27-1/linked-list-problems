7b6412fc376d13e3bc858602767f5fc93147196a
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-04 23:36:55 +0530

    Create reverse.java
    Reversing a linked list.

---
+++ reverse.java
@@ -0,0 +1,12 @@
+static void reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
