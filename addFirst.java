a28b9a2b9ff63a263cb28f274ecbe95735dfb09b
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-04 23:43:01 +0530

    Create addFirst.java
    Adding a node in the starting of linked list.

---
+++ addFirst.java
@@ -0,0 +1,9 @@
+public void addFirst(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
