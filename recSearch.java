5df7f15407098769daa8963cafdccf0b8502508b
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-04 23:37:51 +0530

    Create recSearch.java
    Searching a node using recursive method.

---
+++ recSearch.java
@@ -0,0 +1,16 @@
+static int helper(Node head,int key){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==key){
+            return 0;
+        }
+        int idx=helper(head.next,key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+    static int recSearch(int key){
+        return helper(head,key);
+    }
