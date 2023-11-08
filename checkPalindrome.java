8e2f6180e59516c1499cea3e808400ec667a3bcb
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-08 14:59:00 +0530

    Create checkPalindrome.java
    Check if  linked list is palindrome.

---
+++ checkPalindrome.java
@@ -0,0 +1,25 @@
+public boolean checkPalindrome(){
+        if(head==null || head.next==null){
+            return true;
+        }
+        Node midNode=findMid(head);
+        Node prev=null;
+        Node curr=midNode;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node right=prev;
+        Node left=head;
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
