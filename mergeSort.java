bc73216149504fc5718709738cb3f64626b62ee1
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-11-09 23:09:53 +0530

    Create mergeSort.java
    Sorting a linkedlist using mergeSort.

---
+++ mergeSort.java
@@ -0,0 +1,47 @@
+static Node getmid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    static Node merge(Node head1,Node head2){
+        Node mergeLL=new Node(-1);
+        Node temp=mergeLL;
+        while(head!=null && head.next!=null){
+            if(head.data<=head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while (head1!=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2!=null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return mergeLL.next;
+    }
+    static Node mergeSort(Node head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node mid=getmid(head);
+        Node rightHead=mid.next;
+        mid.next=null;
+        Node newLeft=mergeSort(head);
+        Node newRight=mergeSort(rightHead);
+        return merge(newLeft,newRight);
+    }
